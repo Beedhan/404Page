001764aa7ab8ab1d4171c217fc5749750e8d39a5
Beedhan <bhuwaibidhan@gmail.com>
2021-03-16 14:19:30 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const cat_fact = document.querySelector(".cat-facts");
+
+const people = {A:"DIwash",B:"Beedhan"}
+
+
+const data =  ({A}) => {
+  console.log(A)
+};
+data(people);
+
+// const data =  (p) => {
+//  console.log(p.A)
+// };
+// data(people);
